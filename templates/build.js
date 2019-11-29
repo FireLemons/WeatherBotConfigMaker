c1d63b235f7e70820c6d6bd46bd6cc8199bd9c34
Shen Yang <shenyang64@gmail.com>
2019-11-28 18:07:32 -0600

    initial standalone commit

---
+++ templates/build.js
@@ -0,0 +1,45 @@
+const fs = require('fs').promises
+const Mustache = require('mustache')
+
+let indexTemplate,
+
+    sharedScriptsPartial,
+    sharedStylesPartial
+
+
+function onIndexFilesLoaded(){
+  if(indexTemplate && sharedScriptsPartial && sharedStylesPartial){
+    fs.writeFile('./index.html', Mustache.render(indexTemplate, {}, {
+      'shared-styles': sharedStylesPartial,
+      'shared-scripts': sharedScriptsPartial
+    })).then(() => {
+      console.log('generated index.html(config maker index)')
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+}
+
+fs.readFile('./templates/configMaker.mustache', 'utf8')
+  .then((template) => {
+    indexTemplate = template
+    onIndexFilesLoaded()
+  }).catch((err) => {
+    console.error(err)
+  })
+
+fs.readFile('./templates/sharedScripts.mustache', 'utf8')
+  .then((template) => {
+    sharedScriptsPartial = template
+    onIndexFilesLoaded()
+  }).catch((err) => {
+    console.error(err)
+  })
+
+fs.readFile('./templates/sharedStyles.mustache', 'utf8')
+  .then((template) => {
+    sharedStylesPartial = template
+    onIndexFilesLoaded()
+  }).catch((err) => {
+    console.error(err)
+  })
